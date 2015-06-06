885db6d26112aad47c2cde079d12e0efca77599d
Matt Welch <mattw0308@gmail.com>
2015-06-06 17:10:38 -0600

    First versino of the install script

--- install_script.sh
+++ install_script.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-mkdir ~/results/logs/
-#sudo apt-get update && sudo apt-get upgrade -y
+# make results directories
+mkdir -p ~/results/logs/
+
+# install useful tools
+sudo apt-get update 
 sudo apt-get install -y vim ethtool screen qemu-kvm exuberant-ctags 
+
+# install Docker
+DOCKER=$(which docker)
+
+if [[ -z $DOCKER ]];
+then
+    wget -qO- https://get.docker.com/ | sh
+fi
 

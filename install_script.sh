069bf354ab6baa3d63064c7b15f986abf059c013
Matt Welch <mattw0308@gmail.com>
2015-06-06 17:28:05 -0600

    Added submodule directory

--- install_script.sh
+++ install_script.sh
@@ -5,7 +5,8 @@ mkdir -p ~/results/logs/
 # install useful tools
 sudo apt-get update 
 sudo apt-get install -y vim ethtool screen qemu-kvm exuberant-ctags apparmor
-# apparmor is to enable docker
+# apparmor is to enable docker 
+# http://stackoverflow.com/questions/29294286/fata0000-get-http-var-run-docker-sock-v1-17-version-dial-unix-var-run-doc
 
 # install Docker
 DOCKER=$(which docker)

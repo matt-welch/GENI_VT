f7b552c75c28e5d7e008eaea1621d1f2de965ba6
Matt Welch <mattw0308@gmail.com>
2015-06-06 17:23:21 -0600

    added apparmor install and docker service start.  Docker is running!

--- install_script.sh
+++ install_script.sh
@@ -4,7 +4,8 @@ mkdir -p ~/results/logs/
 
 # install useful tools
 sudo apt-get update 
-sudo apt-get install -y vim ethtool screen qemu-kvm exuberant-ctags 
+sudo apt-get install -y vim ethtool screen qemu-kvm exuberant-ctags apparmor
+# apparmor is to enable docker
 
 # install Docker
 DOCKER=$(which docker)
@@ -12,5 +13,8 @@ DOCKER=$(which docker)
 if [[ -z $DOCKER ]];
 then
     wget -qO- https://get.docker.com/ | sh
+    sudo usermod -aG docker mattwel
+    sudo service docker start
 fi
 
+sudo docker run hello-world

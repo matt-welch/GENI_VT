9b959b4504623d7d19fc15efd21a1d4621167510
Matt Welch <mattw0308@gmail.com>
2015-06-07 18:25:10 -0700

    minor mods to startvm; added download script for ubuntu.img from home NAS - please don't attack me internet!

---
+++ download_vm_image.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget --no-check-certificate -o ubuntu.img https://craft.homeip.net:5001/fbsharing/cJXsilMf
--- startvm.sh
+++ startvm.sh
@@ -3,14 +3,17 @@
 CPUTYPE="host,level=9"
 NAME="node1_vm"
 TELNET_PORT=4445
-
+DEBUG="debug"
+#HUGE="default_hugepagesz=2M hugepagesz=2M hugepages=2048"
+KERNEL="/home/matt/images/vmlinuz-3.16.0-30-generic"
+KERN_OPTS="console=ttyS0 console=tty0 isolcpus=1-3 irqaffinity=0 rcu_nocbs=1-4 rcu_nocb_poll=1 clocksource=tsc tsc=perfect nohz_full=1-3 highres=off ${HUGE} selinux=0 enforcing=0 $DEBUG raid=noautodetect" 
 
 # start the bridge if it's not running
 if [ "`/sbin/ifconfig | grep ^br0`" = "" ]; then
     /usr/bin/startbr.sh
 fi
 
-sudo taskset 0x2 qemu-system-x86_64 \
+qemu-system-x86_64 \
     -enable-kvm \
     -name $NAME \
     -cpu $CPUTYPE \
@@ -24,10 +27,8 @@ sudo taskset 0x2 qemu-system-x86_64 \
     -serial stdio \
     -netdev tap,id=mattap0 \
     -device virtio-net-pci,netdev=mattap0 \
-    -daemonize 
 
-
-reset # terminal gets weird after qemu exits so reset it
+#reset # terminal gets weird after qemu exits so reset it
 
 #
 #  I don't think these work - they may disable the vnc server

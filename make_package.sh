17ea134ac3f7b63dca36a022b6d5b67b8039fab3
Matt Welch <mattw0308@gmail.com>
2015-06-14 19:47:55 -0500

    Moved XML rspecs, mods to make_package and prepare_tunnel.

--- make_package.sh
+++ make_package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 TARBALL="package.tar.bz2"
-CONTENTS="./keys/ ./bootstrap_node.sh ../images/ubuntu.tar.bz2 "
+CONTENTS="keys/ bootstrap_node.sh images/ubuntu.tar.bz2 "
+
 mv ${TARBALL} ${TARBALL}.bak
 
 echo "Making $TARBALL containing {${CONTENTS}}..."
--- prepare_ssh_vnc_tunnel.sh
+++ prepare_ssh_vnc_tunnel.sh
@@ -1,1 +1,5 @@
-ssh -L 5901:localhost:5901 -N -f -l mattwel $(cat nodes.lst | cut -d ' ' -f 1)
+# change the NODE variable bwloe to select node 1 or 2 (from nodes.lst
+NODE=1
+VNCPORT=5901
+USER=mattwel
+ssh -L $VNCPORT:localhost:$VNCPORT -N -f -l $USER $(cat nodes.lst | cut -d ' ' -f $NODE)

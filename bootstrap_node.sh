c6649295e9f3be306c507644c7045abcaedd1023
Matt Welch <mattw0308@gmail.com>
2015-06-14 18:54:25 -0600

    Modifications to node bootstrapper to streamline install process

--- bootstrap_node.sh
+++ bootstrap_node.sh
@@ -6,18 +6,21 @@ mv keys/* ~/.ssh/
 echo "Making results directory..."
 mkdir -p ~/results/logs/
 
-echo "Making images directory..."
-mkdir -p ~/images
-
-echo "Extracting VM image..."
-tar xvf ./images/ubuntu.tar.bz2  -C ~/images
-
 echo "Cloning into GENI_VT..."
 git clone git@github.com:matt-welch/GENI_VT.git
 cd GENI_VT
 git submodule init
 git submodule update
+./install_script.sh
 
+# prepare dotfiles
 cd daffy-dotfiles/
 ./install.sh
+
+# extract VM image 
+echo "Making images directory..."
+mkdir -p ~/images
+
+echo "Extracting VM image..."
+tar xvf ~/images/ubuntu.tar.bz2  -C ~/images
 
--- install_script.sh
+++ install_script.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-# install useful tools
+### install useful tools
 sudo apt-get update 
-sudo apt-get install -y vim ethtool screen qemu-kvm exuberant-ctags apparmor bridge-utils unzip
+sudo apt-get install -y vim ethtool screen qemu-kvm exuberant-ctags apparmor bridge-utils 
 # NOTE: apparmor is to enable docker; 
 # http://stackoverflow.com/questions/29294286/fata0000-get-http-var-run-docker-sock-v1-17-version-dial-unix-var-run-doc
 
 ### Install experiment software
-# git clone git://dpdk.org/apps/pktgen-dpdk
-# git clone git://
-# wget http://dpdk.org/browse/dpdk/snapshot/dpdk-2.0.0.tar.gz
-
 # TODO make it an option to install docker
 
 
-# install Docker
+### make links to start scripts & qemu-ifup
+echo "Preparing vm files..."
+ln -s ~/GENI_VT/startvm.sh ~/images/startvm.sh
+sudo mv /etc/qemu-ifup ./qemu-ifup.orig
+sudo ln -s $(pwd)/qemu-ifup /etc/qemu-ifup
+
+### install Docker
 DOCKER=$(which docker)
 
 if [[ -z $DOCKER ]];
@@ -23,8 +25,14 @@ then
     sudo usermod -aG docker mattwel
     sudo service docker start
 fi
-
 sudo docker run hello-world
 
-# make links to start scripts
-ln -s ~/GENI_VT/startvm.sh ~/images/startvm.sh
+### Download, build dpdk
+# git clone git://dpdk.org/apps/pktgen-dpdk
+echo "Downloading and installing dpdk..."
+wget http://dpdk.org/browse/dpdk/snapshot/dpdk-2.0.0.tar.gz
+tar xvf dpdk-2.0.0.tar.gz
+cd dpdk-2.0.0/
+make config T=x86_64-native-linuxapp-gcc && make
+
+cd ~/GENI_VT/

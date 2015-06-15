5d8982afd5a97c951d3f77216b5cdacf81dc2fda
Matt Welch <mattw0308@gmail.com>
2015-06-14 18:11:53 -0700

    Further modifications for streamlined booting.

--- bootstrap_control.sh
+++ bootstrap_control.sh
@@ -9,6 +9,8 @@ USER="mattwel"
 NODE_IX=1
 if [[ -n $1 ]] ; then
     NODE_IX=$1
+else
+    echo "Call ./$0 <NODE>"
 fi
 HOST=$(cat nodes.lst | cut -d ' ' -f ${NODE_IX})
 TARGETDIR="/users/${USER}/"
@@ -21,5 +23,8 @@ fi
 echo "Transferring $FILE to ${USER}@${HOST}:${TARGETDIR}"
 scp -i $KEY -v $FILE ${USER}@${HOST}:${TARGETDIR}
 
-echo "Transfer complete. SSH to ${USER}@${HOST}:${TARGETDIR}"
+echo "Transfer complete. SSH to ${USER}@${HOST}:${TARGETDIR} & extract package..."
 ssh -i $KEY -v ${USER}@${HOST} 'tar xvf package.tar.bz2'
+
+echo "Package extracted. SSH to ${USER}@${HOST}:${TARGETDIR} & install..."
+ssh -i $KEY -v ${USER}@${HOST} 'sudo ./bootstrap_node.sh'
--- prepare_VNC_tunnel.sh
+++
@@ -1,1 +0,0 @@
-ssh -L 5901:localhost:5901 -N -f -l mattwel pc4.instageni.illinois.edu

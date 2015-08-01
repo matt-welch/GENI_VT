b1f750fd07185cb1d3ee63213f15c07ebd004041
Matt Welch <mattw0308@gmail.com>
2015-08-01 14:43:10 -0600

    Modifying benchmarks. * iperf3 and cyclictest now install in GENI_VT/benchmarks

--- install/install_benchmarks.sh
+++ install/install_benchmarks.sh
@@ -9,7 +9,7 @@ cd $BENCH_DIR
 function install_iperf() {
     THIS_DIR=iperf
     if [[ -d $THIS_DIR ]] ; then
-        cd iperf
+        cd $THIS_DIR
         if [[ -f install_complete ]] ; then
             echo "$THIS_DIR is already installed"
         else
@@ -17,20 +17,32 @@ function install_iperf() {
         fi
     else
         git clone https://github.com/esnet/iperf.git
-        cd iperf
+        cd $THIS_DIR
         ./configure
         make -j $(nproc)
-        sudo make install
+        #sudo make install
+        ln -s /users/mattwel/GENI_VT/benchmarks/iperf/src/iperf3 $BENCH_DIR/iperf3
         date > install_complete
     fi
 }
 
 function install_cyclictest() {
-    exit
-    sudo apt-get install -y build-essential libnuma-dev
-    git clone $KERNELGIT/clrkwllms/rt-tests.git
-    cd rt-tests
-    make -j $(nproc)
+    THIS_DIR=rt-tests
+    if [[ -d $THIS_DIR ]] ; then
+        cd $THIS_DIR
+        if [[ -f install_complete ]] ; then
+            echo "$THIS_DIR is already installed"
+        else
+            echo "An error has occurred - $THIS_DIR installation has failed"
+        fi
+    else
+        sudo apt-get install -y build-essential libnuma-dev
+        git clone $KERNELGIT/clrkwllms/rt-tests.git
+        cd rt-tests
+        make -j $(nproc)
+        ln -s /users/mattwel/GENI_VT/benchmarks/$THIS_DIR/cyclictest $BENCH_DIR/cyclictest
+        date > install_complete
+    fi
 }
 
 for BENCHMARK in ${BENCH_LIST[@]}; do

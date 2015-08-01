cb66b0c3a272455f2680969614edae0de177c122
Matt Welch <mattw0308@gmail.com>
2015-08-01 11:25:46 -0700

    mod to benchmark script so it doesn't create a ~/benchmarks directory

--- install/install_benchmarks.sh
+++ install/install_benchmarks.sh
@@ -2,20 +2,32 @@
 
 KERNELGIT=git://git.kernel.org/pub/scm/linux/kernel/git
 BENCH_LIST="iperf cyclictest "
-BENCH_DIR="~/benchmarks"
+BENCH_DIR="/users/mattwel/GENI_VT/benchmarks"
 mkdir -p $BENCH_DIR
 cd $BENCH_DIR
 
 function install_iperf() {
-    git clone https://github.com/esnet/iperf.git
-    cd iperf
-    ./configure 
-    make -j $(nproc)
-    sudo make install
+    THIS_DIR=iperf
+    if [[ -d $THIS_DIR ]] ; then
+        cd iperf
+        if [[ -f install_complete ]] ; then
+            echo "$THIS_DIR is already installed"
+        else
+            echo "An error has occurred - $THIS_DIR installation has failed"
+        fi
+    else
+        git clone https://github.com/esnet/iperf.git
+        cd iperf
+        ./configure
+        make -j $(nproc)
+        sudo make install
+        date > install_complete
+    fi
 }
 
 function install_cyclictest() {
-    sudo apt-get install -y build-essential libnuma-dev 
+    exit
+    sudo apt-get install -y build-essential libnuma-dev
     git clone $KERNELGIT/clrkwllms/rt-tests.git
     cd rt-tests
     make -j $(nproc)
@@ -26,3 +38,4 @@ for BENCHMARK in ${BENCH_LIST[@]}; do
     echo "Installing $BENCHMARK ..."
     install_${BENCHMARK}
 done
+

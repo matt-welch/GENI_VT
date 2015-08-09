491122b108b191cb60f100528c497dc1ada429d1
Matt Welch <mattw0308@gmail.com>
2015-08-09 17:36:28 -0600

    Trying to start pktgen, but running into LUA issues...argh

--- install/setup_DPDK.sh
+++ install/setup_DPDK.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+PKTGEN="/users/mattwel/dpdk/pktgen-2.9.1/app/app/x86_64-native-linuxapp-gcc/pktgen"
+
+function mount_hugetlbfs () {
+    MOUNTPOINT="/mnt/huge"
+    sudo mkdir -p $MOUNTPOINT
+    ISMOUNTED=$(mount | grep hugetlbfs)
+
+    if [[ -z "$ISMOUNTED" ]] ; then 
+        sudo mount -t hugetlbfs none $MOUNTPOINT
+    fi
+    mount | grep hugetlbfs
+}
+
 function setup_dpdk() {
     cd ~/dpdk
     source RTE_vars.sh
@@ -11,7 +24,7 @@ function setup_dpdk() {
     sudo modprobe uio
     sudo insmod kmod/igb_uio.ko
 
-
+    mount_hugetlbfs 
     # these interfaces will likely not be correct
     ifconfig eth1 down
     ifconfig eth3 down
@@ -27,6 +40,15 @@ function setup_pktgen () {
     source RTE_vars.sh
 
     cd pktgen-2.9.1/
+    mount_hugetlbfs
 
 
 }
+
+function run_pktgen () {
+    ARGS=' -c 0x1e -n 4 -- -p 0x3 -m "2.0,3.1" '
+    COMMAND="$PKTGEN $ARGS"
+    echo $COMMAND
+
+}
+

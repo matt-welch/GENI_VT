fd44547eb75ab60aba6bd96eb3c1d114e5e22fb3
Matt Welch <mattw0308@gmail.com>
2015-08-09 17:01:20 -0600

    Setting up DPDK

--- install/setup_DPDK.sh
+++ install/setup_DPDK.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 function setup_dpdk() {
+    cd ~/dpdk
+    source RTE_vars.sh
+
     # assumes DPDK 2.0.0 is already isntalled
-    cd /users/mattwel/dpdk-2.0.0/
+    cd dpdk-2.0.0/${RTE_TARGET}
+
+    # insert drivers
+    sudo modprobe uio
+    sudo insmod kmod/igb_uio.ko
+
+
     # these interfaces will likely not be correct
     ifconfig eth1 down
     ifconfig eth3 down
@@ -12,6 +21,12 @@ function setup_dpdk() {
     # after checking the status, bind the igb_uio driver to the data (not eth0) interfaces
 }
 
+
 function setup_pktgen () {
+    cd ~/dpdk
+    source RTE_vars.sh
+
+    cd pktgen-2.9.1/
+
 
 }

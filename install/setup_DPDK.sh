f1aea7e43b7ec65e606ca54e5088aa4420de51ac
Matt Welch <mattw0308@gmail.com>
2015-08-09 18:51:00 -0600

    Now instlling DPDK and dpdk-pktgen. * any dpdk app (testpmd, pktgen) seems to break network connectivity for * the control interface too

--- install/setup_DPDK.sh
+++ install/setup_DPDK.sh
@@ -22,16 +22,19 @@ function setup_dpdk() {
 
     # insert drivers
     sudo modprobe uio
+
     sudo insmod kmod/igb_uio.ko
+    lsmod | grep -e uio -e igb
 
     mount_hugetlbfs 
     # these interfaces will likely not be correct
-    ifconfig eth1 down
-    ifconfig eth3 down
-    cd tools/
+    sudo ifconfig eth1 down
+    sudo ifconfig eth3 down
+    cd ${RTE_SDK}/tools/
     ./dpdk_nic_bind.py --status
 
     # after checking the status, bind the igb_uio driver to the data (not eth0) interfaces
+
 }
 
 
@@ -42,11 +45,16 @@ function setup_pktgen () {
     cd pktgen-2.9.1/
     mount_hugetlbfs
 
+    BOUND=$($RTE_SDK/tools/dpdk_nic_bind.py --status | grep drv=igb_uio )
 
+    if [[ -z "$BOUND" ]] ; then 
+        echo "ERROR: No interfaces bound to igb_uio"
+        echo "  use $RTE_SDK/tools/dpdk_nic_bind.py -b igb_uio bb:dd.f "
+    fi
 }
 
 function run_pktgen () {
-    ARGS=' -c 0x1e -n 4 -- -p 0x3 -m "2.0,3.1" '
+    ARGS=' -c 0x1e -n 4 --pci-whitelist 02:00.1 -w 02:00.3 -- -P -l pktgen.log -m "2.0,3.1" '
     COMMAND="$PKTGEN $ARGS"
     echo $COMMAND
 

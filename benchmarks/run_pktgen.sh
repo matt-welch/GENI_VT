bf546fd3e13efde59535aa6dcc09885776c0bb73
Matt Welch <mattw0308@gmail.com>
2015-08-18 14:24:06 -0700

    Added 'eval' to pktgen command so it would parse correctly.  This fixed the pktgen weirdness I'd seen with mapping and cores not receiving.

--- benchmarks/run_pktgen.sh
+++ benchmarks/run_pktgen.sh
@@ -4,14 +4,14 @@ if [ -z "$GENI_HOME" ] ; then
     echo "The GENI_HOME variable has not been defined."
 fi
 
-source ${GENI_HOME}/util/ids.sh
-source ${GENI_HOME}/util/bash_colors.sh
-source ${GENI_HOME}/util/sys_vars.sh
+source $GENI_HOME/util/ids.sh
+source $GENI_HOME/util/bash_colors.sh
+source $GENI_HOME/util/sys_vars.sh
 
-PKTGEN_HOME="${HOMEDIR}/dpdk/pktgen-2.9.1"
-PKTGEN_BIN="${PKTGEN_HOME}/app/app/x86_64-native-linuxapp-gcc/pktgen"
-EAL_ARGS=" -c 0x3e -n 4 --pci-whitelist $IF1_PCI --pci-whitelist $IF2_PCI "
-PKTGEN_ARGS='-- -P -l pktgen.log -m "{4:5}.1, {2:3}.0"'
+PKTGEN_HOME="$HOMEDIR/dpdk/pktgen-2.9.1"
+PKTGEN_BIN="$PKTGEN_HOME/app/app/x86_64-native-linuxapp-gcc/pktgen"
+EAL_ARGS=" -c 0x3e -n 4 --master-lcore=1 "
+PKTGEN_ARGS="-- -p 0x3 -P -l /root/pktgen.log -m \"[2:3].0,[4:5].1\""
 COMMAND="$PKTGEN_BIN $EAL_ARGS $PKTGEN_ARGS"
 
 fcn_print_red "Execute the following command to start pktgen: "
@@ -27,10 +27,10 @@ and the black on black theme will switch to colors on black."
 read -p "Do you want to start the command line above? (y/N): " -n 1 input
 
 if [ "$input" == "y" -o "$input" == "Y" ] ; then 
-    echo "Changing directory to ${PKTGEN_HOME} (so pktgen won't complain about LUA)"
+    echo "Changing directory to $PKTGEN_HOME (so pktgen won't complain about LUA)"
     cd $PKTGEN_HOME 
     fcn_print_red "Starting pktgen ... "
-    $COMMAND
+    eval "$COMMAND"
 fi
 echo
 

51406e9e1246c3a3dfc30c8372861b5e4bf0d869
Matt Welch <mattw0308@gmail.com>
2015-09-07 20:14:17 -0700

    Mods to data collection.  Less to change now.

--- benchmarks/replicate_netperf.sh
+++ benchmarks/replicate_netperf.sh
@@ -6,7 +6,8 @@ VERBOSITY="-v 4 "
 CONFIDENCE="-I 99,5 "
 REQ_RESP_SIZE="-r 1,1 "
 TCP_NODELAY="-D "
-GLOBAL_OPTS=" -C $VERBOSITY -j "
+# show remote CPU usage, keep additional timing stats
+GLOBAL_OPTS=" -C -j "
 REPS=20
 CORE=0x2
 function usage (){
@@ -33,6 +34,7 @@ eval "$COMMAND"
 
 # TCP request/response
 TESTNAME=TCP_RR
+GLOBAL_OPTS="$GLOBAL_OPTS $VERBOSITY"
 for (( i=1 ; i <= $REPS ; i++)) ; do 
     COMMAND="taskset $CORE ./netperf -H $SERVERIP -p $PORT $GLOBAL_OPTS -t $TESTNAME  \
         -- $REQ_RESP_SIZE $TCP_NODELAY"

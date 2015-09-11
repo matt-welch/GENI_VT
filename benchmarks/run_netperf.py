2a6e9435b99becd096b3c6195821211456670ea9
Matt Welch <mattw0308@gmail.com>
2015-09-11 11:12:05 -0700

    Added python script for colelcting and processing netperf data and pexpect.py (v2.4) for python process execution.

--- benchmarks/run_netperf.py
+++ benchmarks/run_netperf.py
@@ -10,15 +10,44 @@ from datetime import datetime
 import threading
 from math import sqrt
 
-#import pexpect (assumes pexpecf.py is in the same directory as script
+#import pexpect (assumes pexpect.py is in the same directory as script
 scriptPath = os.path.abspath(sys.argv[0])
 scriptName = scriptPath.split("/")[-1]
 scriptDirectory = scriptPath.split(scriptName)[0]
+outputDir = "/root/results/"
 imp.load_source('pexpect', '../util/pexpect-2.4/pexpect.py')
 import pexpect
 
+# control/output globals 
 DEBUGMODE=False
 DRYRUN=False
+VERBOSITY=2
+WRITE_OUTPUT=True
+FASTMODE=False
+
+
+# netperf variables: 
+vtType = "docker"
+numReps = 20
+serverPort = 65432
+bridgedIP = "192.168.42.242"
+directPyhsIP = "192.168.3.1"
+interfaceList = [bridgedIP, directPyhsIP]
+testList = [ "TCP_RR", "TCP_STREAM"]# [ "TCP_RR", "TCP_STREAM"]
+keyStr = pexpect.EOF
+confidenceInterval = "-I 99,5"
+testDuration = "" # "-l 1" # "-l 10" # 10 seconds is standard, use 1 for quick debugging 
+if FASTMODE: 
+    numReps=2
+    testDuration="-l 1"
+    interfaceList = [bridgedIP] 
+    testList = ["TCP_RR"]
+
+
+if WRITE_OUTPUT: 
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M")
+    outputFile = (outputDir + vtType + "_" + timestamp + ".dat")
+    fHandle = open(outputFile, 'w')
 
 class Dataset(object):
     def __init__(self, vtType=None, nettype=None, test=None, command=None, rawdata=[], timestamp=None):
@@ -29,7 +58,6 @@ class Dataset(object):
         self.rawdata = rawdata
         self.timestamp = timestamp
 
-
 def buildNetperfCommand(serverIP, serverPort, test): 
     command=""
     if( test == "TCP_RR"): 
@@ -37,17 +65,17 @@ def buildNetperfCommand(serverIP, serverPort, test):
     elif (test == "TCP_STREAM"): 
         command=buildStreamCommand(serverIP, serverPort)
     elif (test == "TCP_RR_CI"): 
-        command=buildRRCommand(serverIP, serverPort, "-I 99,5")
+        command=buildRRCommand(serverIP, serverPort, confidenceInterval)
     elif (test == "TCP_STREAM_CI"): 
-        command=buildStreamCommand(serverIP, serverPort, "-I 99,5")
+        command=buildStreamCommand(serverIP, serverPort, confidenceInterval)
     return command
 
 def buildStreamCommand(serverIP, serverPort, CI=""):
     #  ./netperf -H 192.168.42.242 -p 65432 -v 2 -t TCP_STREAM
     verbosity="-v 2"
     test="-t TCP_STREAM"
-    netperfCmd = ("./netperf -l 1 -H {0} -p {1} {2} {3} {4}").format(serverIP, 
-            serverPort, verbosity, CI, test)
+    netperfCmd = ("./netperf {0} -H {1} -p {2} {3} {4} {5}").format(testDuration, 
+            serverIP, serverPort, verbosity, CI, test)
     return netperfCmd
 
 def buildRRCommand(serverIP, serverPort, CI=""):
@@ -55,8 +83,8 @@ def buildRRCommand(serverIP, serverPort, CI=""):
     verbosity="-v 4"
     test="-t TCP_RR"
     rr_size="-r 1,1"
-    netperfCmd = ("./netperf -l 1 -H {0} -p {1} {2} {3} {4} -- {5} ").format(serverIP, 
-            serverPort, verbosity, CI, test, rr_size )
+    netperfCmd = ("./netperf {0} -H {1} -p {2} {3} {4} {5} -- {6} ").format(testDuration, 
+            serverIP, serverPort, verbosity, CI, test, rr_size )
     return netperfCmd
 
 def runCommand(command, keyStr, timeOut=30): 
@@ -64,12 +92,16 @@ def runCommand(command, keyStr, timeOut=30):
     if not(DRYRUN): 
         localThread = pexpect.spawn(command)
         localThread.expect(keyStr, timeout=timeOut)
-        output = localThread.before + "\n" + "Complete at " + str(datetime.now())
+        if VERBOSITY > 1: 
+            output = localThread.before + "\n" + "Complete at " + str(datetime.now()) + "\n"
     return output
 
-def parseStream(data): 
-    print ("\n{0}\n".format(data.rawdata))
-    return 0 
+def getKernelInfo():
+    command = "uname -a"
+    multiPrint(runCommand(command, pexpect.EOF), 1) 
+    command = "cat /proc/cmdline" 
+    multiPrint(runCommand(command, pexpect.EOF), 1) 
+    return
 
 def mean(data): 
     sum=0.0
@@ -85,76 +117,92 @@ def stddev(data):
         sumres+=(resid * resid)
     return sqrt(sumres / ((len(data)*1.0) - 1.0) )
 
-def calcStats(data):
-    return mean(data), stddev(data)
-
-
-def parseRR(intputData):
-    latency=[]
-    for datum in intputData.rawdata: 
+def parseStream(inputData): 
+    bwList=[]
+    for datum in inputData.rawdata: 
         rows = datum.split("\r\n")
         #for ix,row in enumerate(rows): 
         #    print ix,row 
-        raw = rows[4].split()
-        latency.append(float(raw[4]))
-    avg,std = calcStats(latency)
-    return latency, avg, std
+        raw = rows[6].split()
+        bwList.append(float(raw[4]))
+    return bwList, mean(bwList), stddev(bwList)
+
+def parseRR(inputData):
+    if len(inputData.rawdata) > 1: 
+        latList=[]
+        for datum in inputData.rawdata: 
+            rows = datum.split("\r\n")
+            #for ix,row in enumerate(rows): 
+            #    print ix,row 
+            raw = rows[4].split()
+            latList.append(float(raw[4]))
+    else: 
+        datum=inputData.rawdata
+        rows = datum.split("\r\n")
+        for ix,row in enumerate(rows): 
+            print ix,row 
+        latList=[1,1]
+    return latList, mean(latList), stddev(latList)
+
+def multiPrint(thisString, level=0): 
+    if VERBOSITY > level: 
+        print(thisString)
+    if WRITE_OUTPUT: 
+        fHandle.write(thisString+"\n")
+    return
 
 
-serverPort = 65432
-bridgedIP = "192.168.42.242"
-directPyhsIP = "192.168.3.1"
-keyStr = pexpect.EOF
-
-#command = buildStreamCommand(bridgedIP, serverPort)
-#print(runCommand(command, keyStr) + "\n" )
-#command = buildRRCommand(bridgedIP, serverPort)
-#print(runCommand(command, keyStr) + "\n" )
-#command = buildStreamCommand(directPyhsIP, serverPort)
-#print(runCommand(command, keyStr) + "\n" )
-#command = buildRRCommand(directPyhsIP, serverPort)
-#print(runCommand(command, keyStr) + "\n" )
-
-interfaceList = [bridgedIP] #, directPyhsIP]
-testList = [ "TCP_RR"] #, "TCP_STREAM"]
-vtType = "docker"
-numReps = 4
-results = []
-for interfaceIP in interfaceList:
-    for test in testList: 
-        command = buildNetperfCommand(interfaceIP, serverPort, test)
-        # run each test 20 times
-        rawdata = []
-        for rep in range(numReps): 
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print("{0}: Running test: {1}, {2}, rep {3}/{4}".format(timestamp, interfaceIP, test, rep+1, numReps))
-            print(command)
-            rawdata.append( runCommand(command, keyStr) )
-            time.sleep(0.1) 
-            print(rawdata[-1])
-        results.append(Dataset(vtType, interfaceIP, test, command, rawdata, timestamp ) )
-
-#        # run CI-tests
-#        command = buildNetperfCommand(interfaceIP, serverPort, test+"_CI")
-#        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#        print("{0}: Running test: {1}, {2}, rep {3}/{4}".format(timestamp, interfaceIP, test+"_CI", 1, 1))
-#        print(command )
-#        rawdata.append( runCommand(command, keyStr, 120) )
-#        time.sleep(0.1) 
-#        print(rawdata[-1] )
-#        results.append(Dataset(vtType, interfaceIP, test+"_CI", command, rawdata, timestamp ) )
-
-print(len(results))
-for data in results: 
-    print("vtType: {0}, IP: {1}:{2}, {3} @ {4} ".format(data.vtType, data.nettype, serverPort, data.test, data.timestamp))
-    if data.test == "TCP_STREAM": 
-        parseStream(data)
-    elif data.test == "TCP_RR": 
-        latency, avg, std = parseRR(data)
-        print("Mean={0}, stddev={1}".format(avg, std))
-
-
+def main(): 
+    # TODO this needs to run on the remote host : getKernelInfo()
+    results = []
+    # TODO Add ability to hava a pool of threads running netperf client 
+    for interfaceIP in interfaceList:
+        for test in testList: 
+            command = buildNetperfCommand(interfaceIP, serverPort, test)
+            # run each test 20 times
+            rawdata = []
+            for rep in range(numReps): 
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                multiPrint("{0}: Running test: {1}, {2}, rep {3}/{4}".format(timestamp, interfaceIP, test, rep+1, numReps), 0)
+                multiPrint(command, 1)
+                rawdata.append( runCommand(command, keyStr) )
+                time.sleep(1) 
+                multiPrint(rawdata[-1], 2)
+            results.append(Dataset(vtType, interfaceIP, test, command, rawdata, timestamp ) )
     
+# TODO: need a better parser to handle CI-variants
+#            # run CI-tests
+#            command = buildNetperfCommand(interfaceIP, serverPort, test+"_CI")
+#            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#            multiPrint("{0}: Running test: {1}, {2}, rep {3}/{4}".format(timestamp, interfaceIP, test+"_CI", 1, 1), 0)
+#            multiPrint(command, 1)
+#            rawdata.append( runCommand(command, keyStr, 120) )
+#            time.sleep(1) 
+#            multiPrint(rawdata[-1], 2)
+#            results.append(Dataset(vtType, interfaceIP, test+"_CI", command, rawdata, timestamp ) )
+    
+    print("\n\nResults Summary: ")
+    for data in results: 
+        multiPrint("\nvtType: {0}, IP: {1}:{2}, {3} @ {4} ".format(data.vtType, data.nettype, serverPort, data.test, data.timestamp), 0)
+        if data.test == "TCP_STREAM": 
+            bandwidth, avg, std = parseStream(data)
+            multiPrint("Count={0}, Mean={1}, stddev={2}".format(len(bandwidth), avg, std), 0)
+            multiPrint(str(bandwidth), 1)
+        elif data.test == "TCP_RR": 
+            latency, avg, std = parseRR(data)
+            multiPrint("Count={0}, Mean={1}, stddev={2}".format(len(latency), avg, std), 0)
+            multiPrint(str(latency), 1)
+    
+    multiPrint("\nTesting complete at {0} for {1}, ".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), vtType), 0)
+    multiPrint("Data written to {0}".format(outputFile), 0 )
+    print("")
 
-# TODO Add ability to hava a pool of threads running netperf client 
+    if WRITE_OUTPUT:
+        fHandle.close
+    
+# end main()
+
+if __name__ == '__main__':
+    multiPrint("command line arguments: "+str(sys.argv), 3)
+    main()
 

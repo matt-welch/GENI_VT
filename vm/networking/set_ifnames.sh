095458921ebe656b8bf1e98bfebae9953f2abcf4
Matt Welch <mattw0308@gmail.com>
2015-09-18 03:54:39 -0700

    Better set_ifnames script.

--- vm/networking/set_ifnames.sh
+++ vm/networking/set_ifnames.sh
@@ -1,11 +1,22 @@
 #!/bin/bash 
-OLD=em0
-NEW=eth0
-ip link set $OLD name $NEW 
-OLD=p258p1
+OLD=eth1
 NEW=eth10
+ifconfig $OLD down 
 ip link set $OLD name $NEW 
-OLD=p258p2
+
+OLD=eth4
 NEW=eth11
+ifconfig $OLD down 
 ip link set $OLD name $NEW 
+
+OLD=rename2
+NEW=eth0
+ifconfig $OLD down 
+ip link set $OLD name $NEW 
+
+OLD=eth8
+NEW=eth1
+ifconfig $OLD down 
+ip link set $OLD name $NEW 
+dhclient eth1
 

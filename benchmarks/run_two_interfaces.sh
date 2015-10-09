d7558db9b5216e36b69faf7f7be18ba8769df403
Matt Welch <mattw0308@gmail.com>
2015-10-09 06:38:03 -0700

    cleaned up iperf and netperf scripts.

--- benchmarks/run_two_interfaces.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./run_netperf_fast.sh 192.168.42.245 | tee /root/results/3.18.20/netperf-remote-cont-br.dat 
-
-
-./run_netperf_fast.sh 192.168.3.1 | tee /root/results/3.18.20/netperf-remote-cont-phy.dat 

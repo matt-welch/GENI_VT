d0b2b5c8602ae327d01511eb4eaf98e64089a283
Matt Welch <mattw0308@gmail.com>
2015-10-23 09:15:06 -0700

    Modifications to Dockerfiles and container run scripts.

--- docker/netbench/run_container.sh
+++ docker/netbench/run_container.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 REPO="bench/network"
 TAG="netbench"
-NAME="netbench"
-HWADDR="de:ad:be:ef:42:02"
-MAC="--mac-address $HWADDR "
 RUN_CMD="/bin/bash"
 if [[ $(uname -r) == *"rt18"* ]] ; then 
     PINNING=" --cpuset-cpus=1-4 --cpuset-mems=0"
+    echo "Realtime kernel, Setting pinning to : $PINNING"
 else
     PINNING=""
 fi
 
-docker run --rm=true -it $MAC $PINNING --name=${NAME} ${REPO}:${TAG} $RUN_CMD
+docker run --rm=true -it $PINNING --name=${NAME} ${REPO}:${TAG} $RUN_CMD
 
 
